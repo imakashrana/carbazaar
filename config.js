b5a0d121f34588ab7155ae63a52408bb50c15a78
imakashrana <30438670+imakashrana@users.noreply.github.com>
2018-09-01 13:14:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports ={
-    'database': 'mongodb://localhost:27017/carbazar',
-    //'database': 'mongodb://admin:admin123@ds123562.mlab.com:23562/carbazaar',
+    //'database': 'mongodb://localhost:27017/carbazar',
+    'database': 'mongodb://admin:admin123@ds123562.mlab.com:Welcome@123/carbazaar',
     'secret':'mysecretkey'
  }; 
