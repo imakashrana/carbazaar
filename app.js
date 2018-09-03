4ac91a0341d20d00a87ed9ecc1ceba67e760ef3c
imakashrana <30438670+imakashrana@users.noreply.github.com>
2018-09-03 17:29:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect(config.database)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var localRouter = require('./routes/local');
-var carRouter = require('./routes/car');
+//var carRouter = require('./routes/car');
 var app = express();
 
 // view engine setup
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/car', carRouter);
+//app.use('/car', carRouter);
 app.use('/local', localRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
